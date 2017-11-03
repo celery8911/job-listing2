76768a2cef0543ae24c8bb317ff2560dcc1ab337
Celery8911 <celery8911@gmail.com>
2017-11-03 16:33:43 +0800

    implement step 5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,11 +1,12 @@
 class Admin::JobsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 before_action :require_is_admin
+layout "admin"
 def show
   @job = Job.find(params[:id])
 end
 def index
-  @jobs = Job.where(:is_hidden => false)
+  @jobs = Job.all
 end
 def new
   @job = Job.new
@@ -39,6 +40,21 @@ def destroy
   @job.destroy
   redirect_to admin_jobs_path, alert: "Job deleted!"
 end
+
+def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+
+    @job.hide!
+
+    redirect_to :back
+  end
 
 
 
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,10 @@ class JobsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 def show
   @job = Job.find(params[:id])
+
+  if @job.is_hidden
+    redirect_to root_path, notice: "This job already archived!"
+end
 end
 
 def index
